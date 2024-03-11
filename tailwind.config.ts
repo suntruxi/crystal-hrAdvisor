4292edd15be20eef172be1dcb1c9280434adeab6
alexvieru1 <vieru.alexandruu@gmail.com>
2024-03-11 15:13:45 +0200

    nextjs, tailwind and shadcn install

---
+++ tailwind.config.ts
@@ -0,0 +1,124 @@
+import type { Config } from "tailwindcss"
+
+const config = {
+  darkMode: ["class"],
+  content: [
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+	],
+  prefix: "",
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {
+      colors: {
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+      fontSize: {
+        xxs: "clamp(0.75rem, 0.5vw + 0.5rem, 0.875rem)",
+        xs: "clamp(0.75rem, 0.5vw + 0.5rem, 0.875rem)",
+        sm: "clamp(0.8rem, 0.17vw + 0.76rem, 0.89rem)",
+        md: "clamp(1rem, 0.34vw + 0.91rem, 1.19rem)",
+        lg: "clamp(1.25rem, 0.61vw + 1.1rem, 1.58rem)",
+        xl: "clamp(1.56rem, 1vw + 1.31rem, 2.11rem)",
+        "2xl": "clamp(1.95rem, 1.56vw + 1.56rem, 2.81rem)",
+        "3xl": "clamp(2.44rem, 2.38vw + 1.85rem, 3.75rem)",
+        "4xl": "clamp(3.05rem, 3.54vw + 2.17rem, 5rem)",
+        "5xl": "clamp(3.81rem, 5.18vw + 2.52rem, 6.66rem)",
+        "6xl": "clamp(4.77rem, 7.48vw + 2.9rem, 8.88rem)",
+        "7xl": "clamp(5.96rem, 10.2vw + 3.3rem, 11.5rem)",
+        "8xl": "clamp(7.45rem, 13.8vw + 3.73rem, 14.5rem)",
+        "9xl": "clamp(9.31rem, 17.8vw + 4.19rem, 17.5rem)",
+      },
+      screens: {
+        "4xs": "160px",
+        // => @media (min-width: 160px) { ... }
+
+        "3xs": "240px",
+        // => @media (min-width: 240px) { ... }
+
+        "2xs": "320px",
+        // => @media (min-width: 320px) { ... }
+
+        xs: "480px",
+        // => @media (min-width: 480px) { ... }
+
+        sm: "640px",
+        // => @media (min-width: 640px) { ... }
+
+        md: "768px",
+        // => @media (min-width: 768px) { ... }
+
+        lg: "1024px",
+        // => @media (min-width: 1024px) { ... }
+
+        xl: "1280px",
+        // => @media (min-width: 1280px) { ... }
+
+        "2xl": "1536px",
+        // => @media (min-width: 1536px) { ... }
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
+} satisfies Config
+
+export default config
